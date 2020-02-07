94b63fbd418e6e1a7071441a69d8b45f051bdba1
Raffaele Di Matteo <49187662+redskatex@users.noreply.github.com>
2020-02-07 20:50:36 +0100

    fix

--- database.sql
+++ database.sql
@@ -1,17 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versione server:              5.7.24 - MySQL Community Server (GPL)
--- S.O. server:                  Win64
--- HeidiSQL Versione:            10.3.0.5771
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
 CREATE TABLE IF NOT EXISTS `borsa` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Nome` varchar(50) NOT NULL,
@@ -20,14 +6,5 @@ CREATE TABLE IF NOT EXISTS `borsa` (
   `Min` double NOT NULL,
   `Max` double NOT NULL,
   `Med` double unsigned DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `FK_borsa_items` (`Nome`),
-  CONSTRAINT `FK_borsa_items` FOREIGN KEY (`Nome`) REFERENCES `items` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*!40000 ALTER TABLE `borsa` DISABLE KEYS */;
-/*!40000 ALTER TABLE `borsa` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+  PRIMARY KEY (`ID`)
+)
